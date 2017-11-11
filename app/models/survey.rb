73c0f2163439bf2462c0cdaf09ef278ce82c9dcf
majesty86 <tladislas@gmail.com>
2017-11-11 16:03:44 -0600

    Define survey and questions in controller, fix typo in models to move methods into their given classes

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,14 +2,14 @@ class Survey < ApplicationRecord
   # Remember to create a migration!
   belongs_to :creator, class_name: "User"
   has_many :questions
-end
 
-def survey_stats
-  stat_array = []
-  self.questions.each{|q|
-    stat_array << q.raw_stats
-  }
-  stat_array
-end
+  def survey_stats
+    stat_array = []
+    self.questions.each{|q|
+      stat_array << q.raw_stats
+    }
+    stat_array
+  end
 
+end
 #Note: stat array returns [{answer.id: choice.count},{answer.id: choice.count, answer.id: choice.count}] the question number is going to be the array position + 1
