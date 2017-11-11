18faac09b6d429d0b361a55c2cf24f4e1912dec0
kris <ksoule77@gmail.com>
2017-11-11 17:57:39 -0600

    redux-k

--- app/helpers/pie_chart_helper.js
+++ app/helpers/pie_chart_helper.js
@@ -2,10 +2,10 @@
 window.onload = function () {
   var chart = new CanvasJS.Chart("chartContainer",
   {
-    #Title needs to be the question text for the survey, along with the survey itself. ex: survey.name, question.text
+    // Title needs to be the question text for the survey, along with the survey itself. ex: survey.name, question.text
     title:{
       text: "Survey results"},
-      # this size works for our purposes, although might want to make it a bit bigger.
+      // # this size works for our purposes, although might want to make it a bit bigger.
     legend: {
       maxWidth: 350,
       itemWidth: 120
@@ -15,10 +15,10 @@ window.onload = function () {
       type: "pie",
       showInLegend: true,
       legendText: "{indexLabel}",
-     # [{answer.id: choice.count},{answer.id: choice.count, answer.id: choice.count}]
+     // # [{answer.id: choice.count},{answer.id: choice.count, answer.id: choice.count}]
       dataPoints: [
-     #Each hash in the array is a question. Key is the answer.id, value is the count.
-      #In this case the y: would = the value. index label is the answer.text for answer.id
+     // #Each hash in the array is a question. Key is the answer.id, value is the count.
+      // #In this case the y: would = the value. index label is the answer.text for answer.id
 
         { y: 4181563, indexLabel: "PlayStation 3" },
 
