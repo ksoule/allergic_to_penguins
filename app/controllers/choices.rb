d73ddbe17adf5c8648279aba5e1eddd3a43e7a54
majesty86 <tladislas@gmail.com>
2017-11-11 15:15:36 -0600

    Clean up, psuedo for AJAX

--- app/controllers/choices.rb
+++ app/controllers/choices.rb
@@ -1,12 +1,10 @@
 post '/choices' do 
-  # some weird statkeeping shit here?
-  # params[:possible_choices].each do?  Can I do that?
   user_choices = params[:choice].values
-  ep user_choices
 
   user_choices.each do |answer_id|
     Choice.create(chooser_id: current_user.id, answer_id: answer_id)
   end
 
+  # Will change redirect to erb and implement AJAX.
   redirect '/surveys' 
 end
