307dd4982d8361083d06622c76b6e298923d10b7
kris <ksoule77@gmail.com>
2017-11-13 07:34:18 -0600

    working charts. color choice could improve

--- app/controllers/choices.rb
+++ app/controllers/choices.rb
@@ -1,11 +1,12 @@
-post '/choices' do 
+post '/choices' do
   @survey = Survey.find_by(id: params[:survey_id])
-  @stats = @survey.survey_stats
+
   user_choices = params[:choice].values
 
   user_choices.each do |answer_id|
     Choice.create(chooser_id: current_user.id, answer_id: answer_id)
   end
 
-  erb :'/surveys/_results', layout: false
+  content_type :json
+  @survey.survey_stats.to_json
 end
