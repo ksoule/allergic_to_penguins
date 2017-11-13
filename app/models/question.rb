24018a8b8677b3eded598571377b5ab6820d6ee8
kris <ksoule77@gmail.com>
2017-11-12 19:04:35 -0600

    merge

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,11 +3,11 @@ class Question < ApplicationRecord
   belongs_to :survey
   has_many :answers
 
-#stats per question {answer.id: answer.choices.count,}
-def raw_stats
+#stats per question {answer.text: answer.choices.count,}
+  def raw_stats
     data_set = {}
     self.answers.each{|answer|
-        data_set[answer.id] = answer.choices.count
+        data_set[answer.text] = answer.choices.count
       }
     data_set
   end
