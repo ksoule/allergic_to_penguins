10c0c700d65de9a5928d4d558cfbdf7c68e47cb1
AllyJo <gatorally245@gmail.com>
2017-11-12 22:49:52 -0600

    fix alignment on post route

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -12,9 +12,9 @@ post '/surveys/:survey_id/questions/:question_id/answers' do
   if @answer.save
     if request.xhr?
       erb :'/answers/_form', layout: false
-  else
-    @errors = @answer.errors.full_messages
-    redirect "/surveys/#{@survey.id}/questions/#{@question.id}/answers/new"
+    else
+      @errors = @answer.errors.full_messages
+      redirect "/surveys/#{@survey.id}/questions/#{@question.id}/answers/new"
     end
   end
 end
